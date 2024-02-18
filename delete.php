f7e84ed1c3057cca25c53edd5d4f37e414d46f6a
Dasuri330 <151821656+Dasuri330@users.noreply.github.com>
2024-02-18 16:45:05 +0800

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  include 'database.php';
+
+  //assume that there is delete button and deletebutton id in html
+  if (isset($_GET['deleteid'])){ 
+    $id=$_GET['deleteid'];
+
+    $sql="delete from 'petadoptionlogin' where id=$id";
+    $result=mysqli_query($con,$sql);
+    if($result){
+        header('location:admin.php')// assume that there is a page for admin where he can delete the data
+    }else{
+        die(mysqli_error($con));
+    }
+  }
+*/
+?>
+
